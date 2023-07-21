687d3e53b8b6be3a7c90c50d16c4aef721457e70
aggheorghiu <103059662+aggheorghiu@users.noreply.github.com>
2023-07-22 01:46:52 +0300

    fix3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
 import Portfolio from './Components/Portfolio';
 import Contact from './Components/Contact';
